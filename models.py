8c2f124d3f182ab4f60719d8c372bbec2e35f55a
Singhsansar <singhsansar433@gmail.com>
2024-11-11 16:47:25 +0530

    Made changes in model of blood req

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from patient import models as pmodels
 from donor import models as dmodels
+
 class Stock(models.Model):
     bloodgroup=models.CharField(max_length=10)
     unit=models.PositiveIntegerField(default=0)
