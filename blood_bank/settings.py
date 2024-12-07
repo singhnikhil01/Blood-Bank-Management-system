f74fc5eeed77c4b1e6fea88955137f5e0aaceee5
Singhsansar <singhsansar433@gmail.com>
2024-12-07 08:28:52 +0530

    added static root path to settings.py

--- blood_bank/settings.py
+++ blood_bank/settings.py
@@ -18,8 +18,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-7ke)26fuk+lbo$qg*12v2+l$u*87w%(qv%_(91c80@-za-n(_8"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['blood-bank-management-system-b3nn.onrender.com']
+DEBUG = True
+ALLOWED_HOSTS = ['blood-bank-management-system-b3nn.onrender.com','localhost','127.0.0.1']
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -99,6 +99,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 STATIC_DIR,
  ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL='/afterlogin'
 #for contact us give your gmail id and password
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
